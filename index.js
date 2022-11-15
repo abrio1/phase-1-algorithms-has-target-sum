f194eeafdb187e520415a84b8cac3a3609ea3c6a
Amanda Briones <amandabriones111@gmail.com>
2022-11-15 14:10:26 -0800

    commit tree 0feb1c8c86ccefb08813a33b653a9a306d60b2c1

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
+// example array [19, 4, 22, 10] 27
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  //iterate through each number in the array
+  for (let i = 0; i < array.length; i++) {
+  //for the current num, identify a complement that adds to the target (complement = target - current num)
+  const complement = target - array[i]
+  //iterate through the rest of the array
+  for (let j = i + 1; j <array.length; j++) {
+  //check if any number is out complement
+  //is so, return true
+  if (array[j] === complement) return true;
+    }
+  }
+  //if I reach the end of the array, return false
+return false;
 }
 
+// O(n) runtime
+function findSock(array) {
+  for (const item of array) {
+    if (item === "sock") return "sock";
+  }
+}
 /* 
-  Write the Big O time complexity of your function here
+  Write the Big O time complexity of your function here:
+  Runtime: O(n^2) pronounced O of n squared
+  Space: O(n) o of n space complexity
 */
 
 /* 
   Add your pseudocode here
+  hasTargetSum([22, 19, 4, 6, 30], 25)
+  iterate through each number in the array
+  for the current num, identify a complement that adds to the target (complement = target - current num)
+    check if any number is out complement
+    is fo, return true
+
+  if I reach the end of the array, return false
 */
 
 /*
